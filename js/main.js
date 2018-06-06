db21e43941a4f54d548834b47cda251a77b68200
Dean <d.chalk@students.rave.ac.uk>
2018-06-06 13:14:29 +0100

    uploading new site

--- js/main.js
+++ js/main.js
@@ -1,70 +1,43 @@
-(function(){
-	function VerticalTimeline( element ) {
-		this.element = element;
-		this.blocks = this.element.getElementsByClassName("js-cd-block");
-		this.images = this.element.getElementsByClassName("js-cd-img");
-		this.contents = this.element.getElementsByClassName("js-cd-content");
-		this.offset = 0.8;
-		this.hideBlocks();
-	};
+// Get the video
+var video = document.getElementById("myVideo");
+var enterButton = $("#enterButton");
+var fullscreen = $("#fullscreen-bg");
+var logo = $("#logoBig");
+var flex = $("#flex-container");
+var header = $("header");
+var upload = $("#upload");
+var uploadLink = $("#uploadLink");
+var festivalsLink = $("#festivalsLink");
 
-	VerticalTimeline.prototype.hideBlocks = function() {
-		//hide timeline blocks which are outside the viewport
-		if ( !"classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
-					self.images[i].classList.add("cd-is-hidden"); 
-					self.contents[i].classList.add("cd-is-hidden"); 
-				}
-			})(i);
-		}
-	};
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
-	VerticalTimeline.prototype.showBlocks = function() {
-		if ( ! "classList" in document.documentElement ) {
-			return;
-		}
-		var self = this;
-		for( var i = 0; i < this.blocks.length; i++) {
-			(function(i){
-				if( self.contents[i].classList.contains("cd-is-hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
-					// add bounce-in animation
-					self.images[i].classList.add("cd-timeline__img--bounce-in");
-					self.contents[i].classList.add("cd-timeline__content--bounce-in");
-					self.images[i].classList.remove("cd-is-hidden");
-					self.contents[i].classList.remove("cd-is-hidden");
-				}
-			})(i);
-		}
-	};
+enterButton.click(function (){
+    fullscreen.hide();
+    logo.fadeOut(250);
+    enterButton.fadeOut(100);
+    $(".flex-item").fadeIn(500);
+    header.fadeIn(500);
+});
 
-	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
-		verticalTimelinesArray = [],
-		scrolling = false;
-	if( verticalTimelines.length > 0 ) {
-		for( var i = 0; i < verticalTimelines.length; i++) {
-			(function(i){
-				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
-			})(i);
-		}
+uploadLink.click(function () {
+   upload.fadeIn(250);
+    $(".flex-item").fadeOut(150);
+});
 
-		//show timeline blocks on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
-			}
-		});
-	}
+festivalsLink.click(function () {
+    $(".flex-item").fadeIn(250);
+    upload.fadeOut(150);
+});
 
-	function checkTimelineScroll() {
-		verticalTimelinesArray.forEach(function(timeline){
-			timeline.showBlocks();
-		});
-		scrolling = false;
-	};
-})();
+$(function() {
+  $(".tintImg").each(function() {
+  $(this).wrap('<div class="tint"></div>');
+  });
+});
